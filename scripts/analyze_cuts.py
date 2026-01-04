45e332a4315d0a3f245c0033e97d4073c78e8e83
CristopherErazo <cris.erv.98@gmail.com>
2026-01-04 18:37:07 +0100

    Include filtering based on bimodality

--- scripts/analyze_cuts.py
+++ scripts/analyze_cuts.py
@@ -2,6 +2,8 @@ import argparse
 import numpy as np
 from configurations import make_params_dict ,load_data , save_data
 from scipy.stats import zscore
+from diptest import diptest
+from sklearn.mixture import GaussianMixture
 
 
 if __name__ == "__main__":
@@ -34,8 +36,8 @@ if __name__ == "__main__":
     names_variable = ['T','alpha']
 
     results = {
-        'means' : np.zeros((len(h_values),len(T_values),len(alpha_values),6)),
-        'stds' : np.zeros((len(h_values),len(T_values),len(alpha_values),6)),
+        'means' : np.zeros((len(h_values),len(T_values),len(alpha_values),3)),
+        'stds' : np.zeros((len(h_values),len(T_values),len(alpha_values),3)),
         'counts' : np.zeros((len(h_values),len(T_values),len(alpha_values)))
     }
   
@@ -52,25 +54,52 @@ if __name__ == "__main__":
                 # Filtering based on Dkl values
                 Dkl = x_valid[:,4]
                 print(f'Max Dkl before filtering: {np.max(Dkl)}, total valid points: {len(Dkl)}')
-                Dkl_percentile = np.percentile(Dkl, perc, axis=None)  # 80th percentile across all values
-                mask_Dkl = Dkl <= Dkl_percentile
-                x_filtered = x_valid[mask_Dkl]
+                # If there are -inf in Dkl, we need to handle them
+                # Count how many -inf are there
+                num_neg_inf = np.sum(Dkl == -np.inf)
+                if num_neg_inf == len(Dkl):
+                    print('All Dkl values are -inf, keeping all this parameter set.')
+                    x_filtered = x_valid
+                else:
+                    # Compute percentile ignoring -inf
+                    Dkl_percentile = np.percentile(Dkl[Dkl != -np.inf], perc)
+                    # Create a mask to keep only those below the percentile and the -inf values
+                    mask_Dkl = (Dkl <= Dkl_percentile) | (Dkl == -np.inf)
+                    x_filtered = x_valid[mask_Dkl]
                 print(f'Max Dkl after filtering: {np.max(x_filtered[:,4])}, total filtered points: {len(x_filtered)}')
 
-                 # Check if there is variation across realizations (w) and filter out outliers based on z-score
-                th0 = x_filtered[:,2]/N
+
+                # Test for unimodality in the data along the firts dimension 
+                q = x_filtered[:,0]
+                dip, p_value = diptest(q)
+                print(f'Dip test result: dip={dip}, p-value={p_value}')
+                if p_value > 0.05:
+                    print('Unimodal Data')
+                    x_majority = x_filtered
+                else:
+                    gmm = GaussianMixture(n_components=2, random_state=0)
+                    gmm.fit(q.reshape(-1, 1))
+                    labels = gmm.predict(q.reshape(-1, 1))
+                    counts = np.bincount(labels)
+                    print(f'Component counts: {counts}')
+                    majority_label = np.argmax(counts)
+                    x_majority = x_filtered[labels == majority_label]
+                    print(f'Multimodal Data: selected majority component with {len(x_majority)} points out of {len(x_filtered)}')
+                
+                # Check if there is variation across realizations (w) and filter out outliers based on z-score
+                th0 = x_majority[:,2]/N
                 std_bid = np.std(th0)
                 if std_bid > 1e-3:
                     z_bid = np.abs(zscore(th0))
                     mask_bid = (z_bid < z_th) # Keep only those within threshold
-                    x_final = x_filtered[mask_bid]
+                    x_final = x_majority[mask_bid]
                 else:
-                    x_final = x_filtered
+                    x_final = x_majority
                 
                 print(f'Total points after z-score filtering: {len(x_final)}')
 
-                means = np.mean(x_final, axis=0) #shape (6,)
-                stds = np.std(x_final, axis=0)  #shape (6,)
+                means = np.mean(x_final[:,:3], axis=0) #shape (3,)
+                stds = np.std(x_final[:,:3], axis=0)  #shape  (3,)
 
                 results['means'][ih,iT,ia,:] = means
                 results['stds'][ih,iT,ia,:] = stds
